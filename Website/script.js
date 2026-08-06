e33734b1e646e839e6964db09c158c3a986f042b
Arshdeep Singh <chahalarsh342@gmail.com>
2026-08-06 15:59:19 +0530

    Remaking the website. This is yet just a skeleton

---
+++ Website/script.js
@@ -0,0 +1,15 @@
+document.getElementById('work1').addEventListener('click', function () {
+  window.open("https://example.com", "_blank");
+});
+
+document.getElementById('work2').addEventListener('click', function () {
+  window.open("https://example.com", "_blank");
+});
+
+document.getElementById('work3').addEventListener('click', function () {
+  window.open("https://example.com", "_blank");
+});
+
+document.getElementById('logo').addEventListener('click', function () {
+  window.location.href = 'https://chahalarsh.github.io/chahalarsh';
+});
